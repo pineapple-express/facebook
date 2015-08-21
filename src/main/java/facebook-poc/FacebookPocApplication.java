0b975e8b3b8e76d32380ac413f7f30eb2730c874
Alana Cable <alana.cable@bancvue.com>
2015-08-21 10:12:48 -0500

    Initial commit

---
+++ src/main/java/facebook-poc/FacebookPocApplication.java
@@ -0,0 +1,12 @@
+package facebook-poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacebookPocApplication.class, args);
+    }
+}
